e541897e66cc7cfb31d8c0e6edb0ef4e637094d0
Slipzter <timhanssonmeng@gmail.com>
2023-09-11 23:15:19 +0200

    feat: frontend styling

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -47,7 +47,7 @@ function App() {
       headers:  {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: `${inputType1}=${numbers.toLocaleString()}&nthlargestnumber=${nthNumber}`,
+      body: `${inputType1}=${numbers.toLocaleString()}&${inputType2}=${nthNumber}`,
     })
       .then((response) => response.text())
       .then((data) => {
@@ -64,40 +64,49 @@ function App() {
   
 
   return (
-    <main className='main'>
+    <div className='container'>
       <header className='main__header'>
         <h1 className='main__title'>Schoolsoft Coding Exercise</h1>
       </header>
-      <section className='input-top'>
-        <input
-          type="text"
-          placeholder="Write Here"
-          value={userData}
-          onChange={(e) => setUserData(e.target.value)}
-        />
-        <div className='top-buttons'>
-          <button className='button' onClick={() => getRequest('/users')}>Get Users</button>
-          <button className='button' onClick={() => getRequest('/userswithreversednames')}>Get Users With Reversed First Names</button>
-          <button className='button' onClick={() => postRequest('/reversestring', 'string')}>Reverse Your Input</button>
-          <button className='button' onClick={() => postRequest('/ispalindrome', 'string')}>Check If Your Input Is A Palindrome</button>
-          <button className='button' onClick={() => postRequest('/padnumberwithzeroes', 'number')}>Pad Number With Zeroes</button>
-          <button className='button' onClick={clear}>Clear</button>
-        </div>
-      </section>
-      <input
-        type="text"
-        placeholder="Nth Number Here"
-        value={nthNumber}
-        onChange={(e) => setNthNumber(e.target.value)}
-      />
-      <button onClick={() => postRequest2('/findnthlargestnumber', 'numbers', 'nthlargestnumber')}>Find The Nth Largest Number</button>
-      <article id="apiResponse">{apiResponse}</article>
+      <main className='main__content'>
+        <section className='input-top'> 
+          <input
+            type="text"
+            placeholder="Write Here"
+            value={userData}
+            onChange={(e) => setUserData(e.target.value)}
+            className='input-top__field'
+          />
+          <div className='top-buttons'>
+            <div>
+              <button className='button' onClick={() => postRequest('/reversestring', 'string')}>Reverse Your Input</button>
+              <button className='button' onClick={() => postRequest('/ispalindrome', 'string')}>Check If Your Input Is A Palindrome</button>
+            </div>
+              <button className='button' onClick={() => postRequest('/padnumberwithzeroes', 'number')}>Pad Number With Zeroes</button>
+              <button className='button' onClick={clear}>Clear</button>
+              <input
+                type="text"
+                placeholder="Nth Number Here"
+                value={nthNumber}
+                onChange={(e) => setNthNumber(e.target.value)}
+              />
+             <button onClick={() => postRequest2('/findnthlargestnumber', 'numbers', 'nthlargestnumber')}>Find The Nth Largest Number</button>
+          </div>
+        </section>
+        <aside className="main__output">
+          <div>
+            <button className='button' onClick={() => getRequest('/users')}>Get Users</button>
+            <button className='button' onClick={() => getRequest('/userswithreversednames')}>Get Users With Reversed First Names</button>
+          </div>
+          {apiResponse}
+          </aside>
+      </main>
       <footer className='main__footer'>
         <h5 className='dev-name'>Tim Hansson Meng</h5>
-        <a href="https://github.com/Slipzter"><img className='github-logo' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" /></a>
-        <a href="https://github.com/Slipzter"><img className='linkedin-logo' src="https://img.shields.io/badge/LinkedIn-blue?style=for-the-badge&logo=linkedin&logoColor=white" /></a>
+        <a className='link' href="https://github.com/Slipzter"><img className='github-logo' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" /></a>
+        <a className='link' href="https://github.com/Slipzter"><img className='linkedin-logo' src="https://img.shields.io/badge/LinkedIn-blue?style=for-the-badge&logo=linkedin&logoColor=white" /></a>
       </footer>
-    </main>
+    </div>
   );
 }
 
